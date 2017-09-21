e7d3c44029a63aea4e95d4c170ee4e7420e40b57
banusion <banusion69@free.fr>
2017-09-21 22:16:54 +0200

    add api connector to resolve heroes

--- heroes/src/app/app.module.ts
+++ heroes/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }        from './component/app.component';
 import { HeroDetailComponent } from './component/hero-detail.component';
 import { HeroesComponent }     from './component/heroes.component';
 import { DashboardComponent }  from './component/dashboard.component';
-
-import { AppRoutingModule }     from './app-routing.module';
 
 import { HeroService }         from './service/hero.service';
 
@@ -15,13 +20,15 @@ import { HeroService }         from './service/hero.service';
   imports:      [ 
   	BrowserModule,
   	FormsModule,
-    AppRoutingModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule,
   ],
   declarations: [ 
   	AppComponent,
   	HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   providers: [
     HeroService
