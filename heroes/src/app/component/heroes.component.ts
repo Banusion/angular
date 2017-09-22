70e141a8c863d50c3f277424ba2e8f3a3d7294f1
banusion <banusion69@free.fr>
2017-09-22 08:43:47 +0200

    new function to add or delete heroes

--- heroes/src/app/component/heroes.component.ts
+++ heroes/src/app/component/heroes.component.ts
@@ -12,15 +12,15 @@ import { HeroService } from '../service/hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
-  
+
   constructor(
     private router: Router,
     private heroService: HeroService) { }
-  
+
   getHeroes(): void {
   	this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
-  
+
   onSelect(hero: Hero): void {
   	this.selectedHero = hero;
   }
@@ -29,7 +29,26 @@ export class HeroesComponent implements OnInit {
   	this.getHeroes();
   }
 
-  gotoDetail(): void {
+  goDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
   }
 }
--- heroes/src/app/service/hero.service.ts
+++ heroes/src/app/service/hero.service.ts
@@ -10,7 +10,7 @@ import { Hero } from '../class/hero';
 export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
- 
+
   constructor(private http: Http) { }
 
   getHeroes(): Promise<Hero[]> {
@@ -19,7 +19,7 @@ export class HeroService {
              .then(response => response.json().data as Hero[])
              .catch(this.handleError);
 }
- 
+
 private handleError(error: any): Promise<any> {
   console.error('An error occurred', error); // for demo purposes only
   return Promise.reject(error.message || error);
@@ -40,4 +40,30 @@ private handleError(error: any): Promise<any> {
       .catch(this.handleError);
   }
 
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
+
+  delete(id: number) : Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 }
